243186b99c793a4ce8bc231ec7e34f5d3888d377
saichaitanya3635 <saichaitanya3635@gmail.com>
2025-02-03 16:59:20 +0000

    intital commit

---
+++ src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome()
+    {
+               return "welcome to springboot";
+    }
+    
+}
